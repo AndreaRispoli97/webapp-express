bbedbf515a4ec43a2fdc4ac22906b2c37693b6df
AndreaRispoli97 <Andrea9715@outlook.it>
2025-05-15 17:55:21 +0200

    fix: add error logging in index function for better debugging

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -16,6 +16,7 @@ function index(req, res) {
     connection.query(sql, preparedParams, (err, results) => {
 
         if (err) {
+            console.log(err)
             return res.status(500).json({
                 errorMessage: 'Error connection'
             })
